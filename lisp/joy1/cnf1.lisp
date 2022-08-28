bc5a0beea45206030adcb1b11612d5e2ac3e253c
sazare <song.of.sand@gmail.com>
2022-08-29 08:25:21 +0900

    symbol's value is itself, then no use '. recursive macro application works. see cnf1.lisp

--- lisp/joy1/cnf1.lisp
+++ lisp/joy1/cnf1.lisp
@@ -1,6 +1,21 @@
 ; 1st step of cnf tran
 
+;; def for symbols
+;; for these defs, ',a needless. and the recursive application is effective.
 
+(defvar x 'x)
+(defvar y 'y)
+(defvar a 'a)
+(defvar b 'b)
+(defvar c 'c)
+
+(defvar p 'p)
+(defvar q 'q)
+(defvar r 'r)
+(defvar s 's)
+
+
+;;
 (defun ∧ (a b)
   `(∧ ,a ,b)
 )
@@ -13,16 +28,22 @@
   `(¬ ,a)
 )
 
+
+;(defmacro ≡ (a b)
+;  `(∧ (∨ (¬ ,a) ,b)(∨ ,a (¬ ,b)))
+;)
+
 (defmacro ≡ (a b)
-  `(∧ (∨ (¬ ',a) ',b)(∨ ',a (¬ ',b)))
+  `(∧ (⇒ ,a ,b) (⇒ ,b ,a))
 )
 
 (defmacro ⇒ (a b)
-  `(∨ (¬ ',a) ',b)
+  `(∨ (¬ ,a) ,b)
 )
 
 
 ;; macro dont recursive applied?
+;; but don't use ' and symbol's value is itself, seems allright
 
 (⇒ (⇒ p q) r)
 
